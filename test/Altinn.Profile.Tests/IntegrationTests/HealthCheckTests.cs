d3fb4d36d27dd89b70d73531d2e67afcf4a64290
Stephanie Buadu <47737608+acn-sbuad@users.noreply.github.com>
2022-09-22 14:27:35 +0200

    updated folder and namespaces (#39)

---
+++ test/Altinn.Profile.Tests/IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,38 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Altinn.Profile.Health;
+using Altinn.Profile.Tests.IntegrationTests.Utils;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using Xunit;
+
+namespace Altinn.Profile.Tests.IntegrationTests
+{
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<HealthCheck>>
+    {
+        private readonly WebApplicationFactorySetup<HealthCheck> _webApplicationFactorySetup;
+
+        public HealthCheckTests(WebApplicationFactory<HealthCheck> factory)
+        {
+            _webApplicationFactorySetup = new WebApplicationFactorySetup<HealthCheck>(factory);
+        }
+
+        [Fact]
+        public async Task GetHealth_ReturnsOk()
+        {
+            // Arrange
+            HttpClient client = _webApplicationFactorySetup.GetTestServerClient();
+
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "/health");
+
+            // Act
+            HttpResponseMessage response = await client.SendAsync(httpRequestMessage);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
