3decabb383416f0766dac580623f348692463014
acn-sbuad <stephanie.buadu@avanade.com>
2024-02-28 14:40:05 +0100

    added endpoint and service supporting contactpoint lookup

---
+++ src/Altinn.Profile/Models/UserContactPoints.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Altinn.Profile.Models
+{
+    /// <summary>
+    /// Class describing the availability of contact points for a user
+    /// </summary>
+    public class UserContactPoints
+    {
+        /// <summary>
+        /// Gets or sets the ID of the user
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the national identityt number of the user
+        /// </summary>
+        public string NationalIdentityNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets a boolean indicating whether the user has reserved themselves from electronic communication
+        /// </summary>
+        public bool IsReserved { get; set; }
+
+        /// <summary>
+        /// Gets or sets the mobile number
+        /// </summary>
+        public string MobileNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address
+        /// </summary>
+        public string Email { get; set; }
+    }
+
+    /// <summary>
+    /// A list representation of <see cref="UserContactPoints"/>
+    /// </summary>
+    public class UserContactPointsList
+    {
+        /// <summary>
+        /// A list containing contact points for users
+        /// </summary>
+        public List<UserContactPoints> List { get; set; } = [];
+    }
+}
---
+++ src/Altinn.Profile/Services/Interfaces/IUserContactPoints.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Altinn.Profile.Models;
+
+namespace Altinn.Profile.Services.Interfaces
+{
+    /// <summary>
+    /// Class describing the methods required for user contact point service
+    /// </summary>
+    public interface IUserContactPoints
+    {
+        /// <summary>
+        /// Method for retriveing contact points for a user 
+        /// </summary>
+        /// <param name="nationalIdentityNumbers">A list of national identity numbers to lookup contact points for</param>
+        /// <returns>The users' contact points and reservation status</returns>
+        Task<UserContactPointsList> GetContactPoints(List<string> nationalIdentityNumbers);
+
+        /// <summary>
+        /// Method for retriveing information about the availability of contact points for a user 
+        /// </summary>
+        /// <param name="nationalIdentityNumbers">A list of national identity numbers to look up availability for</param>
+        /// <returns>Information on the existense of the users' contact points and reservation status</returns>
+        Task<UserContactPointAvailabilityList> GetContactPointAvailability(List<string> nationalIdentityNumbers);
+    }
+}
