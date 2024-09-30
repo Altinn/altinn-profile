7bc77bae8160b1d02dce2a6a67d38e97989019da
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2024-09-30 17:13:59 +0200

    Extract UserRegisteredContactPoint to separate class

--- src/Altinn.Profile.Integrations/SblBridge/Unit.Profile/PartyNotificationContactPoints.cs
+++ src/Altinn.Profile.Integrations/SblBridge/Unit.Profile/PartyNotificationContactPoints.cs
@@ -48,28 +48,4 @@ namespace Altinn.Profile.Integrations.SblBridge.Unit.Profile
             return new UnitContactPointsList() { ContactPointsList = contactPoints };
         }
     }
-    
-    /// <summary>
-    /// Model describing the contact information that a user has associated with a party they can represent.
-    /// </summary>
-    public class UserRegisteredContactPoint
-    {
-        /// <summary>
-        /// Gets or sets the legacy user id for the owner of this party notification endpoint.
-        /// </summary>
-        /// <remarks>
-        /// This was named as legacy for consistency. Property for UUID will probably never be added.
-        /// </remarks>
-        public int LegacyUserId { get; set; }
-
-        /// <summary>
-        /// Gets or sets the email address for this contact point.
-        /// </summary>
-        public string Email { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Gets or sets the mobile number for this contact point.
-        /// </summary>
-        public string MobileNumber { get; set; } = string.Empty;
-    }
 }
