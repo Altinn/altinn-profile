643d984767ae12ebb71bf8e9dc30881afc880a6b
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2024-10-08 07:33:05 +0200

    Implement two unit tests to validate the functionality of the user contact details retrieval use case.

---
+++ test/Altinn.Profile.Tests/Profile.Integrations/UserContactDetailsRetrieverTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+using Altinn.Profile.Core;
+using Altinn.Profile.Integrations.Entities;
+using Altinn.Profile.Integrations.Services;
+using Altinn.Profile.Models;
+using Altinn.Profile.UseCases;
+
+using Moq;
+
+using Xunit;
+
+namespace Altinn.Profile.Tests.Profile.Integrations;
+
+public class UserContactDetailsRetrieverTests
+{
+    private readonly Mock<IPersonService> _mockRegisterService;
+    private readonly UserContactDetailsRetriever _retriever;
+
+    public UserContactDetailsRetrieverTests()
+    {
+        _mockRegisterService = new Mock<IPersonService>();
+        _retriever = new UserContactDetailsRetriever(_mockRegisterService.Object);
+    }
+
+    [Fact]
+    public async Task RetrieveAsync_ThrowsArgumentNullException_WhenLookupCriteriaIsNull()
+    {
+        // Act & Assert
+        await Assert.ThrowsAsync<ArgumentNullException>(() => _retriever.RetrieveAsync(null));
+    }
+
+    [Fact]
+    public async Task RetrieveAsync_ReturnsFalse_WhenNationalIdentityNumbersIsEmpty()
+    {
+        // Arrange
+        var lookupCriteria = new UserContactPointLookup { NationalIdentityNumbers = [] };
+
+        // Act
+        var result = await _retriever.RetrieveAsync(lookupCriteria);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task RetrieveAsync_ReturnsFalse_WhenNoContactDetailsFound()
+    {
+        // Arrange
+        var lookupCriteria = new UserContactPointLookup
+        {
+            NationalIdentityNumbers = new List<string> { "08119043698" }
+        };
+
+        _mockRegisterService.Setup(s => s.GetUserContactAsync(lookupCriteria.NationalIdentityNumbers)).ReturnsAsync(false);
+
+        // Act
+        var result = await _retriever.RetrieveAsync(lookupCriteria);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+    }
+}
