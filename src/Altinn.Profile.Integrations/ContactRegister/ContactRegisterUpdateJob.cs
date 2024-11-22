ac4f4510d92ff168c4c6b06f4ecdcb23561a1bd7
Ahmed-Ghanam <114151129+Ahmed-Ghanam@users.noreply.github.com>
2024-11-22 11:24:23 +0100

    Bug fix/improve logging logic (#272)
    * Update the logic to return immediately and not wait for the result of the synchronization process
    
    * Implement simple logging
    
    * Remove the LogWarning and LogInformation calls.
    
    * Return 500 when an exception occur during synchronizing the changes in contact information.
    
    * Execute the asynchronous operation in a separate thread and immediately returns Ok to the client.
    
    * Simplify the SyncChanges method to ensure only one instance of the synchronization task will be executed at a time
    
    * Fix a typo

--- src/Altinn.Profile.Integrations/ContactRegister/ContactRegisterUpdateJob.cs
+++ src/Altinn.Profile.Integrations/ContactRegister/ContactRegisterUpdateJob.cs
@@ -1,63 +1,67 @@
 ﻿using Altinn.Profile.Integrations.Repositories;
 
-namespace Altinn.Profile.Integrations.ContactRegister
+namespace Altinn.Profile.Integrations.ContactRegister;
+
+/// <summary>
+/// An implementation of the <see cref="IContactRegisterUpdateJob"/> interface that will retrieve 
+/// changes from the source registry and update the local contact information.
+/// </summary>
+/// <param name="contactRegisterSettings">Settings for the synchronization update job</param>
+/// <param name="contactRegisterHttpClient">A HTTP client that can be used to retrieve contact details changes</param>
+/// <param name="metadataRepository">A repository implementation for managing persistence of the job status between runs</param>
+/// <param name="personUpdater">A repository implementation for managing persistence for the local contact information</param>
+public class ContactRegisterUpdateJob(
+    ContactRegisterSettings contactRegisterSettings,
+    IContactRegisterHttpClient contactRegisterHttpClient,
+    IMetadataRepository metadataRepository,
+    IPersonUpdater personUpdater)
+    : IContactRegisterUpdateJob
 {
+    private readonly ContactRegisterSettings _contactRegisterSettings = contactRegisterSettings;
+    private readonly IContactRegisterHttpClient _contactRegisterHttpClient = contactRegisterHttpClient;
+    private readonly IMetadataRepository _metadataRepository = metadataRepository;
+    private readonly IPersonUpdater _personUpdater = personUpdater;
+
     /// <summary>
-    /// An implementation of the <see cref="IContactRegisterUpdateJob"/> interface that will retrieve 
-    /// changes from the source registry and update the local contact information.
+    /// Retrieves all changes from the source registry and updates the local contact information.
     /// </summary>
-    /// <param name="contactRegisterSettings">Settings for the synchronization update job</param>
-    /// <param name="contactRegisterHttpClient">A HTTP client that can be used to retrieve contact details changes</param>
-    /// <param name="metadataRepository">A repository implementation for managing persistance of the job status between runs</param>
-    /// <param name="personUpdater">A repository implementation for managing persistance for the local contact information</param>
-    public class ContactRegisterUpdateJob(
-        ContactRegisterSettings contactRegisterSettings,
-        IContactRegisterHttpClient contactRegisterHttpClient,
-        IMetadataRepository metadataRepository,
-        IPersonUpdater personUpdater)
-        : IContactRegisterUpdateJob
+    /// <returns>A task representing the asynchronous operation.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when the endpoint URL is null or empty.</exception>
+    public async Task SyncContactInformationAsync()
     {
-        private readonly ContactRegisterSettings _contactRegisterSettings = contactRegisterSettings;
-        private readonly IContactRegisterHttpClient _contactRegisterHttpClient = contactRegisterHttpClient;
-        private readonly IMetadataRepository _metadataRepository = metadataRepository;
-        private readonly IPersonUpdater _personUpdater = personUpdater;
-
-        /// <summary>
-        /// Retrieves all changes from the source registry and updates the local contact information.
-        /// </summary>
-        /// <returns>A task representing the asynchronous operation</returns>
-        public async Task SyncContactInformationAsync()
+        if (string.IsNullOrWhiteSpace(_contactRegisterSettings.ChangesLogEndpoint))
         {
-            if (string.IsNullOrWhiteSpace(_contactRegisterSettings.ChangesLogEndpoint))
-            {
-                throw new InvalidOperationException("The endpoint URL must not be null or empty.");
-            }
-
-            long finalGlobalChangeNumber;
-            long lastChangeNumberInBatch;
-            do
-            {
-                long previousChangeNumber = await _metadataRepository.GetLatestChangeNumberAsync();
-
-                ContactRegisterChangesLog registerChanges =
-                    await _contactRegisterHttpClient.GetContactDetailsChangesAsync(
-                        _contactRegisterSettings.ChangesLogEndpoint, previousChangeNumber);
-
-                int synchronizedRowCount = 
-                    await _personUpdater.SyncPersonContactPreferencesAsync(registerChanges);
-
-                // setting it high to escape the loop if something is wrong
-                lastChangeNumberInBatch = long.MaxValue; 
-                if (synchronizedRowCount > 0 && registerChanges.EndingIdentifier.HasValue)
-                {
-                    lastChangeNumberInBatch = registerChanges.EndingIdentifier.Value;
-                    await _metadataRepository.UpdateLatestChangeNumberAsync(lastChangeNumberInBatch);
-                }
-
-                // defaulting to -1 to escape the loop if something is wrong
-                finalGlobalChangeNumber = registerChanges.LatestChangeIdentifier ?? -1;
-            }
-            while (lastChangeNumberInBatch < finalGlobalChangeNumber);
+            throw new InvalidOperationException("The endpoint URL must not be null or empty.");
         }
+
+        long finalGlobalChangeNumber;
+        long lastProcessedChangeNumber;
+
+        do
+        {
+            long previousChangeNumber = await _metadataRepository.GetLatestChangeNumberAsync();
+
+            ContactRegisterChangesLog changesLog = await _contactRegisterHttpClient.GetContactDetailsChangesAsync(_contactRegisterSettings.ChangesLogEndpoint, previousChangeNumber);
+
+            if (changesLog == null)
+            {
+                break;
+            }
+
+            int updatedRowsCount = await _personUpdater.SyncPersonContactPreferencesAsync(changesLog);
+
+            if (updatedRowsCount > 0 && changesLog.EndingIdentifier.HasValue)
+            {
+                lastProcessedChangeNumber = changesLog.EndingIdentifier.Value;
+                await _metadataRepository.UpdateLatestChangeNumberAsync(lastProcessedChangeNumber);
+            }
+            else
+            {
+                break;
+            }
+
+            finalGlobalChangeNumber = changesLog.LatestChangeIdentifier ?? lastProcessedChangeNumber;
+        }
+        while (lastProcessedChangeNumber < finalGlobalChangeNumber);
     }
 }
--- src/Altinn.Profile/Controllers/TriggerController.cs
+++ src/Altinn.Profile/Controllers/TriggerController.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 using Altinn.Profile.Integrations.ContactRegister;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Altinn.Profile.Controllers;
 
@@ -14,13 +16,15 @@ namespace Altinn.Profile.Controllers;
 /// Initializes a new instance of the <see cref="TriggerController"/> class.
 /// </remarks>
 /// <param name="contactRegisterUpdateJob">The service for retrieving the contact details.</param>
+/// <param name="logger">A logger to log detailed information.</param>
 [ApiController]
 [ApiExplorerSettings(IgnoreApi = true)]
 [Consumes("application/json")]
 [Produces("application/json")]
 [Route("profile/api/v1/trigger")]
-public class TriggerController(IContactRegisterUpdateJob contactRegisterUpdateJob) : ControllerBase
+public class TriggerController(IContactRegisterUpdateJob contactRegisterUpdateJob, ILogger<TriggerController> logger) : ControllerBase
 {
+    private readonly ILogger<TriggerController> _logger = logger;
     private readonly IContactRegisterUpdateJob _contactRegisterUpdateJob = contactRegisterUpdateJob;
 
     /// <summary>
@@ -29,13 +33,24 @@ public class TriggerController(IContactRegisterUpdateJob contactRegisterUpdateJo
     /// <returns>
     /// A task that represents the asynchronous operation. If successful, returns a status 200 OK.
     /// </returns>
-    /// <response code="200">Starting the synchronisation work was successfull.</response>
+    /// <response code="200">Starting the synchronization work was successful.</response>
+    /// <response code="500">An error occurred while starting the synchronization.</response>
     [HttpGet("syncpersonchanges")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult> SyncChanges()
     {
-        await _contactRegisterUpdateJob.SyncContactInformationAsync();
+        try
+        {
+            await _contactRegisterUpdateJob.SyncContactInformationAsync();
 
-        return Ok();
+            return Ok("Synchronization has completed.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred during the background synchronization.");
+
+            return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while synchronizing the changes.");
+        }
     }
 }
