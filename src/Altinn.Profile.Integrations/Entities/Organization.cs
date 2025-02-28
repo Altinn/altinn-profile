09186674456c08b9763ad8def76dff7b2d934568
Sigrid Bratsberg <sigrid.bratsberg@digdir.no>
2025-02-28 14:08:07 +0100

    Sort and rmeove using

--- src/Altinn.Profile.Integrations/Entities/Organization.cs
+++ src/Altinn.Profile.Integrations/Entities/Organization.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Altinn.Profile.Integrations.Entities
 {
     /// <summary>
     /// class for organizations connection id and orgNumber
     /// </summary>
-    [Table("OfficialContactPoints", Schema = "organization_contact_info")]
+    [Table("Organizations", Schema = "organization_contact_info")]
 
     public class Organization
     {
