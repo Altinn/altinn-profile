870a52ce7ea3bd73d3535386e6277c349b8ae232
Sigrid Bratsberg <sigrid.bratsberg@digdir.no>
2025-03-06 14:59:08 +0100

    Update to file based namespace declaration

--- src/Altinn.Profile.Integrations/OrganizationNotificationAddress/EntryContent.cs
+++ src/Altinn.Profile.Integrations/OrganizationNotificationAddress/EntryContent.cs
@@ -1,135 +1,134 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Altinn.Profile.Integrations.OrganizationNotificationAddress
+namespace Altinn.Profile.Integrations.OrganizationNotificationAddress;
+
+/// <summary>
+/// The content of the notification address.
+/// </summary>
+public record EntryContent
 {
     /// <summary>
-    /// The content of the notification address.
+    /// The content of the contact point.
     /// </summary>
-    public record EntryContent
+    [JsonPropertyName("Kontaktinformasjon")]
+    public ContactPointModel? ContactPoint { get; init; }
+
+    /// <summary>
+    /// The content of the contact point.
+    /// </summary>
+    public record ContactPointModel
     {
         /// <summary>
-        /// The content of the contact point.
+        /// The identificator of the contact point.
         /// </summary>
-        [JsonPropertyName("Kontaktinformasjon")]
-        public ContactPointModel? ContactPoint { get; init; }
+        [JsonPropertyName("identifikator")]
+        public string? Id { get; init; }
 
         /// <summary>
         /// The content of the contact point.
         /// </summary>
-        public record ContactPointModel
+        [JsonPropertyName("digitalVarslingsinformasjon")]
+        public DigitalContactPointModel? DigitalContactPoint { get; init; }
+
+        /// <summary>
+        /// The content of the contact point.
+        /// </summary>
+        [JsonPropertyName("kontaktinformasjonForEnhet")]
+        public UnitContactInfoModel? UnitContactInfo { get; init; }
+
+        /// <summary>
+        /// The content of the contact point.
+        /// </summary>
+        public record DigitalContactPointModel
         {
             /// <summary>
-            /// The identificator of the contact point.
+            /// The email address of the contact point.
             /// </summary>
-            [JsonPropertyName("identifikator")]
-            public string? Id { get; init; }
+            [JsonPropertyName("epostadresse")]
+            public EmailAddressModel? EmailAddress { get; init; }
 
             /// <summary>
-            /// The content of the contact point.
+            /// The mobile phone number of the contact point.
             /// </summary>
-            [JsonPropertyName("digitalVarslingsinformasjon")]
-            public DigitalContactPointModel? DigitalContactPoint { get; init; }
+            [JsonPropertyName("mobiltelefon")]
+            public PhoneNumberModel? PhoneNumber { get; init; }
 
             /// <summary>
-            /// The content of the contact point.
+            /// The full the email address model.
             /// </summary>
-            [JsonPropertyName("kontaktinformasjonForEnhet")]
-            public UnitContactInfoModel? UnitContactInfo { get; init; }
-
-            /// <summary>
-            /// The content of the contact point.
-            /// </summary>
-            public record DigitalContactPointModel
+            public record EmailAddressModel
             {
                 /// <summary>
-                /// The email address of the contact point.
+                /// The full the email address.
                 /// </summary>
-                [JsonPropertyName("epostadresse")]
-                public EmailAddressModel? EmailAddress { get; init; }
+                [JsonPropertyName("navn")]
+                public string? Name { get; init; }
 
                 /// <summary>
-                /// The mobile phone number of the contact point.
+                /// The domain name of the email address.
                 /// </summary>
-                [JsonPropertyName("mobiltelefon")]
-                public PhoneNumberModel? PhoneNumber { get; init; }
+                [JsonPropertyName("domenenavn")]
+                public string? Domain { get; init; }
 
                 /// <summary>
-                /// The full the email address model.
+                /// The username of the email address.
                 /// </summary>
-                public record EmailAddressModel
-                {
-                    /// <summary>
-                    /// The full the email address.
-                    /// </summary>
-                    [JsonPropertyName("navn")]
-                    public string? Name { get; init; }
-
-                    /// <summary>
-                    /// The domain name of the email address.
-                    /// </summary>
-                    [JsonPropertyName("domenenavn")]
-                    public string? Domain { get; init; }
-
-                    /// <summary>
-                    /// The username of the email address.
-                    /// </summary>
-                    [JsonPropertyName("brukernavn")]
-                    public string? Username { get; init; }
-                }
-
-                /// <summary>
-                /// The full the phone number model.
-                /// </summary>
-                public record PhoneNumberModel
-                {
-                    /// <summary>
-                    /// The full phone number.
-                    /// </summary>
-                    [JsonPropertyName("navn")]
-                    public string? Number { get; init; }
-
-                    /// <summary>
-                    /// The international prefix of the phone number.
-                    /// </summary>
-                    [JsonPropertyName("internasjonaltPrefiks")]
-                    public string? Prefix { get; init; }
-
-                    /// <summary>
-                    /// The national phone number.
-                    /// </summary>
-                    [JsonPropertyName("nasjonaltNummer")]
-                    public string? NationalNumber { get; init; }
-                }
+                [JsonPropertyName("brukernavn")]
+                public string? Username { get; init; }
             }
 
             /// <summary>
-            /// The identifier of the unit of the contact point.
+            /// The full the phone number model.
             /// </summary>
-            public record UnitContactInfoModel
+            public record PhoneNumberModel
             {
                 /// <summary>
-                /// The identifier of the unit of the contact point.
+                /// The full phone number.
                 /// </summary>
-                [JsonPropertyName("enhetsidentifikator")]
-                public UnitIdentifierModel? UnitIdentifier { get; init; }
+                [JsonPropertyName("navn")]
+                public string? Number { get; init; }
 
                 /// <summary>
-                /// The identificator of the unit.
+                /// The international prefix of the phone number.
                 /// </summary>
-                public record UnitIdentifierModel
-                {
-                    /// <summary>
-                    /// The value of the identificator of the contact point.
-                    /// </summary>
-                    [JsonPropertyName("verdi")]
-                    public string? Value { get; init; }
+                [JsonPropertyName("internasjonaltPrefiks")]
+                public string? Prefix { get; init; }
 
-                    /// <summary>
-                    /// The type of identifier.
-                    /// </summary>
-                    [JsonPropertyName("type")]
-                    public string? Type { get; init; }
-                }
+                /// <summary>
+                /// The national phone number.
+                /// </summary>
+                [JsonPropertyName("nasjonaltNummer")]
+                public string? NationalNumber { get; init; }
+            }
+        }
+
+        /// <summary>
+        /// The identifier of the unit of the contact point.
+        /// </summary>
+        public record UnitContactInfoModel
+        {
+            /// <summary>
+            /// The identifier of the unit of the contact point.
+            /// </summary>
+            [JsonPropertyName("enhetsidentifikator")]
+            public UnitIdentifierModel? UnitIdentifier { get; init; }
+
+            /// <summary>
+            /// The identificator of the unit.
+            /// </summary>
+            public record UnitIdentifierModel
+            {
+                /// <summary>
+                /// The value of the identificator of the contact point.
+                /// </summary>
+                [JsonPropertyName("verdi")]
+                public string? Value { get; init; }
+
+                /// <summary>
+                /// The type of identifier.
+                /// </summary>
+                [JsonPropertyName("type")]
+                public string? Type { get; init; }
             }
         }
     }
