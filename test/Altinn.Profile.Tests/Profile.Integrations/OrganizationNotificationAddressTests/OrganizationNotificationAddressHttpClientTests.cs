c0d7fe77b24a87e7073e8b0e325aa18eb7a006ad
Sigrid Bratsberg <sigrid.bratsberg@digdir.no>
2025-03-07 08:03:42 +0100

    Update tests with elegatingHandlerStub

--- test/Altinn.Profile.Tests/Profile.Integrations/OrganizationNotificationAddressTests/OrganizationNotificationAddressHttpClientTests.cs
+++ test/Altinn.Profile.Tests/Profile.Integrations/OrganizationNotificationAddressTests/OrganizationNotificationAddressHttpClientTests.cs
@@ -5,7 +5,10 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Altinn.Platform.Profile.Models;
 using Altinn.Profile.Integrations.OrganizationNotificationAddress;
+using Altinn.Profile.Tests.IntegrationTests.Mocks;
+using Altinn.Profile.Tests.Testdata;
 using Moq;
 using Moq.Protected;
 using Xunit;
@@ -18,78 +21,77 @@ public class OrganizationNotificationAddressHttpClientTests
 
     [Fact]
     public async Task GetAddressChangesAsync_WhenMissingEndpointUrl_Throws()
-    {
-        var httpClient = new HttpClient(_messageHandler.Object);
+    { 
+        // Arrange
+        var httpClient = new HttpClient();
         var client = new OrganizationNotificationAddressHttpClient(httpClient);
 
+        // Act and Assert
         await Assert.ThrowsAsync<ArgumentException>(async () => await client.GetAddressChangesAsync(null));
     }
 
     [Fact]
     public async Task GetAddressChangesAsync_WhenEmptyEndpointUrl_Throws()
     {
-        var httpClient = new HttpClient(_messageHandler.Object);
+        // Arrange
+        var httpClient = new HttpClient();
         var client = new OrganizationNotificationAddressHttpClient(httpClient);
 
+        // Act and Assert
         await Assert.ThrowsAsync<ArgumentException>(async () => await client.GetAddressChangesAsync(string.Empty));
     }
 
     [Fact]
     public async Task GetAddressChangesAsync_WhenInvalidEndpointUrl_Throws()
     {
-        var httpClient = new HttpClient(_messageHandler.Object);
+        // Arrange
+        var httpClient = new HttpClient();
         var client = new OrganizationNotificationAddressHttpClient(httpClient);
 
+        // Act and Assert
         await Assert.ThrowsAsync<ArgumentException>(async () => await client.GetAddressChangesAsync("notAnUrl"));
     }
 
     [Fact]
     public async Task GetAddressChangesAsync_WhenFailingToDeserialize_Throws()
     {
+        // Arrange
         NotificationAddressChangesLog changelog = new NotificationAddressChangesLog();
         var mockResponse = new HttpResponseMessage
         {
             StatusCode = HttpStatusCode.OK,
             Content = JsonContent.Create<NotificationAddressChangesLog>(changelog)
         };
+        DelegatingHandlerStub messageHandler = new((request, cancellationToken) => Task.FromResult<HttpResponseMessage>(mockResponse));
 
-        _messageHandler.Protected()
-        .Setup<Task<HttpResponseMessage>>(
-            "SendAsync",
-            ItExpr.Is<HttpRequestMessage>(m => m.Method == HttpMethod.Get),
-            ItExpr.IsAny<CancellationToken>())
-        .ReturnsAsync(mockResponse);
-
-        var httpClient = new HttpClient(_messageHandler.Object);
+        var httpClient = new HttpClient(messageHandler);
         var client = new OrganizationNotificationAddressHttpClient(httpClient);
 
+        // Act and Assert
         await Assert.ThrowsAsync<OrganizationNotificationAddressChangesException>(async () => await client.GetAddressChangesAsync("http://example.com"));
     }
 
     [Fact]
     public async Task GetAddressChangesAsync_WhenNotSuccessResponse_Throws()
     {
+        // Arrange
         var mockResponse = new HttpResponseMessage
         {
             StatusCode = HttpStatusCode.NotFound,
         };
+        DelegatingHandlerStub messageHandler = new((request, cancellationToken) => Task.FromResult<HttpResponseMessage>(mockResponse));
 
-        _messageHandler.Protected()
-    .Setup<Task<HttpResponseMessage>>(
-        "SendAsync",
-        ItExpr.Is<HttpRequestMessage>(m => m.Method == HttpMethod.Get),
-        ItExpr.IsAny<CancellationToken>())
-    .ReturnsAsync(mockResponse);
-
-        var httpClient = new HttpClient(_messageHandler.Object);
+        var httpClient = new HttpClient(messageHandler);
         var client = new OrganizationNotificationAddressHttpClient(httpClient);
 
+        // Act and Assert
         await Assert.ThrowsAsync<OrganizationNotificationAddressChangesException>(async () => await client.GetAddressChangesAsync("http://example.com"));
     }
 
     [Fact]
     public async Task GetAddressChangesAsync_WhenValidEndpointUrl_Success()
     {
+        // Arrange
         NotificationAddressChangesLog changelog = new NotificationAddressChangesLog { OrganizationNotificationAddressList = [] };
         var mockResponse = new HttpResponseMessage
         {
@@ -97,18 +99,15 @@ public class OrganizationNotificationAddressHttpClientTests
             Content = JsonContent.Create<NotificationAddressChangesLog>(changelog)
         };
 
-        _messageHandler.Protected()
-    .Setup<Task<HttpResponseMessage>>(
-        "SendAsync",
-        ItExpr.Is<HttpRequestMessage>(m => m.Method == HttpMethod.Get),
-        ItExpr.IsAny<CancellationToken>())
-    .ReturnsAsync(mockResponse);
+        DelegatingHandlerStub messageHandler = new((request, cancellationToken) => Task.FromResult<HttpResponseMessage>(mockResponse));
 
-        var httpClient = new HttpClient(_messageHandler.Object);
+        var httpClient = new HttpClient(messageHandler);
         var client = new OrganizationNotificationAddressHttpClient(httpClient);
 
+        // Act
         var va = await client.GetAddressChangesAsync("http://example.com");
 
+        // Assert
         _messageHandler.VerifyAll();
     }
 }
