58c6c263a57a97a29fb1353e9d091d27584dd7a0
Sigrid Bratsberg <sigrid.bratsberg@digdir.no>
2025-03-11 09:57:12 +0100

    First draft of repository

---
+++ src/Altinn.Profile.Integrations/Repositories/OrganizationNotificationAddressUpdater.cs
@@ -0,0 +1,135 @@
+﻿using Altinn.Profile.Integrations.Entities;
+using Altinn.Profile.Integrations.OrganizationNotificationAddressRegistry;
+using Altinn.Profile.Integrations.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace Altinn.Profile.Integrations.Repositories;
+
+/// <inheritdoc />
+public class OrganizationNotificationAddressUpdater(IDbContextFactory<ProfileDbContext> contextFactory) : IOrganizationNotificationAddressUpdater
+{
+    private readonly IDbContextFactory<ProfileDbContext> _contextFactory = contextFactory;
+    private readonly string organizationNumberConst = "ORGANISASJONSNUMMER";
+
+    /// <inheritdoc />
+    public async Task<int> SyncNotificationAddressesAsync(NotificationAddressChangesLog organizationNotificationAddressChanges)
+    {
+        var addresses = organizationNotificationAddressChanges.OrganizationNotificationAddressList!;
+        foreach (var address in addresses) 
+        {
+            if (address.IsDeleted == true)
+            {
+                await DeleteNotificationAddressAsync(address);
+            }
+            else
+            {
+                await UpdateNotificationAddressAsync(address);
+            }
+        }
+    }
+
+    public async Task<int> DeleteNotificationAddressAsync(Entry address)
+    {
+        using ProfileDbContext databaseContext = await _contextFactory.CreateDbContextAsync();
+
+        var entry = await databaseContext.NotificationAddresses.FirstOrDefaultAsync(a => a.RegistryID == address.Id);
+        if (entry == null)
+        {
+            return 0;
+        }
+
+        databaseContext.Remove(entry);
+
+        return await databaseContext.SaveChangesAsync();
+    }
+
+    public async Task<int> UpdateNotificationAddressAsync(Entry address)
+    {
+        var organization = await GetOrganization(address.Content.ContactPoint.UnitContactInfo.UnitIdentifier.Type, address.Content.ContactPoint.UnitContactInfo.UnitIdentifier.Value);
+        if (organization == null)
+        {
+            // Add new organization
+        }
+        else
+        {
+            var existingAddress = organization.NotificationAddresses?.FirstOrDefault(a => a.RegistryID == address.Id);
+            if (existingAddress == null)
+            {
+                // Add address
+            }
+            else
+            {
+            }
+        }
+        
+        var organizationNotificationAddress = new OrganizationNotificationAddress
+        {
+            RegistryID = address.Id,
+            RegistryOrganizationID = string.Empty,
+            RegistryOrganizationNumber = address.Content.ContactPoint.UnitContactInfo.UnitIdentifier.Value,
+            AddressType = adress.
+
+        }
+        using ProfileDbContext databaseContext = await _contextFactory.CreateDbContextAsync();
+
+        var entry = await databaseContext.NotificationAddresses.FirstOrDefaultAsync(a => a.RegistryID == address.Id);
+        if (entry == null)
+        {
+            databaseContext.NotificationAddresses.Add(organizationNotificationAddress);
+        }
+
+        
+        
+        
+
+        return await databaseContext.SaveChangesAsync();
+    }
+
+    private async Task<Organization?> GetOrganization(string identificatortype, string orgNumber)
+    {
+        using ProfileDbContext databaseContext = await _contextFactory.CreateDbContextAsync();
+
+        if (identificatortype == organizationNumberConst)
+        {
+            return await databaseContext.Organizations.FirstOrDefaultAsync(o => o.RegistryOrganizationNumber == orgNumber);
+        }
+
+        return null;
+    }
+
+    private OrganizationNotificationAddress MapOrganizationNotificationAddress(Entry entry)
+    {
+        var organizationNotificationAddress = new OrganizationNotificationAddress
+        {
+            RegistryID = entry.Id,
+            RegistryOrganizationID = 0, // TODO
+            RegistryUpdatedDateTime = entry.Updated,
+            UpdateSource = UpdateSource.KoFuVi,
+            HasRegistryAccepted = true,
+            IsSoftDeleted = false,
+            NotificationName = entry.Title,
+        };
+
+        var contanctPoint = entry.Content.ContactPoint.DigitalContactPoint;
+        var isEmail = contanctPoint.EmailAddress != null;
+
+        if (isEmail)
+        {
+            organizationNotificationAddress.AddressType = AddressType.Email;
+            organizationNotificationAddress.Domain = contanctPoint.EmailAddress.Domain;
+            organizationNotificationAddress.Address = contanctPoint.EmailAddress.Username;
+            organizationNotificationAddress.FullAddress = contanctPoint.EmailAddress.Name;
+
+        }
+        else
+        {
+            organizationNotificationAddress.AddressType = AddressType.SMS;
+            organizationNotificationAddress.Domain = contanctPoint.PhoneNumber.Prefix;
+            organizationNotificationAddress.Address = contanctPoint.PhoneNumber.NationalNumber;
+            organizationNotificationAddress.FullAddress = contanctPoint.PhoneNumber.Number;
+        }
+
+        return organizationNotificationAddress;
+    }
+}
---
+++ src/Altinn.Profile.Integrations/Repositories/RegistrySyncMetadataRepository.cs
@@ -0,0 +1,45 @@
+﻿using Altinn.Profile.Integrations.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Altinn.Profile.Integrations.Repositories;
+
+/// <summary>
+/// Defines a repository for operations related to registry sync metadata.
+/// </summary>
+public class RegistrySyncMetadataRepository(IDbContextFactory<ProfileDbContext> contextFactory) : IRegistrySyncMetadataRepository
+{
+    private readonly IDbContextFactory<ProfileDbContext> _contextFactory = contextFactory;
+
+    /// <inheritdoc />
+    public async Task<DateTime> GetLatestSyncTimestampAsync()
+    {
+        using ProfileDbContext databaseContext = _contextFactory.CreateDbContext();
+
+        var lastSync = await databaseContext.RegistrySyncMetadata.FirstOrDefaultAsync();
+        if (lastSync == null)
+        {
+            return DateTime.MinValue;
+        }
+
+        return lastSync.LastChangedDateTime;
+    }
+
+    /// <inheritdoc />
+    public async Task<DateTime> UpdateLatestChangeTimestampAsync(DateTime updated)
+    {
+        using ProfileDbContext databaseContext = _contextFactory.CreateDbContext();
+        var lastSync = await databaseContext.RegistrySyncMetadata.FirstOrDefaultAsync();
+        if (lastSync == null)
+        {
+            lastSync = new Entities.RegistrySyncMetadata
+            {
+                LastChangedId = Guid.NewGuid().ToString("N"),
+            };
+        }
+
+        lastSync.LastChangedDateTime = updated;
+
+        await databaseContext.SaveChangesAsync();
+        return updated;
+    }
+}
