b86119a5f1b08bf434a923fac8a5e1be5a48a0d3
Sigrid Bratsberg <sigrid.bratsberg@digdir.no>
2025-04-30 13:27:41 +0200

    Reomve duplicate method

--- src/Altinn.Profile.Integrations/OrganizationNotificationAddressRegistry/DataMapper.cs
+++ src/Altinn.Profile.Integrations/OrganizationNotificationAddressRegistry/DataMapper.cs
@@ -152,27 +152,5 @@ namespace Altinn.Profile.Integrations.OrganizationNotificationAddressRegistry
 
             return organizationNotificationAddress;
         }
-
-        /// <summary>
-        /// Maps from the core data model to the data model stored in the database
-        /// </summary>
-        public static NotificationAddressDE MapFromCoreModelNotificationAddress(OrganizationDE organization, NotificationAddress notificationAddress, string registryId)
-        {
-            var organizationNotificationAddress = new NotificationAddressDE
-            {
-                RegistryOrganizationId = organization.RegistryOrganizationId,
-                UpdateSource = UpdateSource.Altinn,
-                HasRegistryAccepted = false,
-                IsSoftDeleted = false,
-                Address = notificationAddress.Address,
-                Domain = notificationAddress.Domain,
-                FullAddress = notificationAddress.FullAddress,
-                AddressType = notificationAddress.AddressType,
-                NotificationName = notificationAddress.NotificationName,
-                RegistryID = registryId,
-            };
-
-            return organizationNotificationAddress;
-        }
     }
 }
