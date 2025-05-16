eb5d6b63e4fcbc37039ac0768a909e6758bee19c
Sigrid <sigbrats@gmail.com>
2025-05-16 15:22:54 +0200

    Add DB schema and tables for groups of parties (#369)
    * Draft new database entities
    
    * Add migration script
    
    * Fix typos
    
    * Rewrite to record and include required modifier
    
    * Add blankline

---
+++ src/Altinn.Profile.Integrations/Entities/Group.cs
@@ -0,0 +1,32 @@
+﻿namespace Altinn.Profile.Integrations.Entities;
+
+/// <summary>
+/// A group of parties organized by a user
+/// </summary>
+public record Group
+{
+    /// <summary>
+    /// The group id
+    /// </summary>
+    public int GroupId { get; set; }
+
+    /// <summary>
+    /// The group name
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// The id of the user owning the group
+    /// </summary>
+    public int UserId { get; set; }
+
+    /// <summary>
+    /// Indicating whether or not he group is a favorite-group
+    /// </summary>
+    public bool IsFavorite { get; set; }
+
+    /// <summary>
+    /// A collection of parties in this group
+    /// </summary>
+    public List<PartyGroupAssociation>? Parties { get; set; }
+}
