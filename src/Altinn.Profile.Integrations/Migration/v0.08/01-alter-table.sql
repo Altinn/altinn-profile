4c5e21fb7cb961e06f96ac5f0ef4eeae5c4ddb54
Sigrid <sigrid.bratsberg@digdir.no>
2025-05-21 14:31:41 +0200

    Add migration script

---
+++ src/Altinn.Profile.Integrations/Migration/v0.08/01-alter-table.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE user_preferences.party_group_association DROP COLUMN party_id;
+
+ALTER TABLE user_preferences.party_group_association ADD party_uuid uuid NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000';
