dea7d8a4e14c189dd32f8d2eba6f95a09a1b42fb
Sigrid <sigrid.bratsberg@digdir.no>
2025-05-23 11:36:27 +0200

    Update test to theory

--- test/Altinn.Profile.Tests/Profile/Validators/NotificationAddressModelTests.cs
+++ test/Altinn.Profile.Tests/Profile/Validators/NotificationAddressModelTests.cs
@@ -72,10 +72,17 @@ namespace Altinn.Profile.Tests.Profile.Validators
             Assert.NotEmpty(validationResult);
         }
 
-        [Fact]
-        public void NotificationAddressModel_WhenInvalidNorwegianPhoneIsGiven_ReturnsValidationResults()
+        [Theory]
+        [InlineData("12345678")]
+        [InlineData("22345678")]
+        [InlineData("32345678")]
+        [InlineData("52345678")]
+        [InlineData("62345678")]
+        [InlineData("72345678")]
+        [InlineData("82345678")]
+        public void NotificationAddressModel_WhenNorwegianPhoneNotLedBy4Or9_ReturnsValidationResults(string phone)
         {
-            var model = new NotificationAddressModel { CountryCode = "+47", Phone = "19876543" };
+            var model = new NotificationAddressModel { CountryCode = "+47", Phone = phone };
             var validationContext = new ValidationContext(model);
 
             var validationResult = model.Validate(validationContext);
