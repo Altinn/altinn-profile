7d7900c0dc1ff2e4e3aad80d2cce99d0412486a0
Terje Holene <terje.holene@gmail.com>
2025-06-17 15:50:35 +0200

    update xml documentation

--- src/Altinn.Profile/Controllers/UnitContactPointController.cs
+++ src/Altinn.Profile/Controllers/UnitContactPointController.cs
@@ -6,39 +6,49 @@ using Altinn.Profile.Core.Unit.ContactPoints;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Altinn.Profile.Controllers
+namespace Altinn.Profile.Controllers;
+
+/// <summary>
+/// This controller provides endpoint for accessing user registered notification addresses for organizations.
+/// The notification addresses are typically registered by users in a professional context.
+/// </summary>
+/// <remarks>
+/// The endpoints are intended for internal consumption (e.g. Notifications) and do not require authenticated
+/// user token or access token authorization.
+/// </remarks>
+[Route("profile/api/v1/units/contactpoint")]
+[ApiExplorerSettings(IgnoreApi = true)]
+[Consumes("application/json")]
+[Produces("application/json")]
+public class UnitContactPointController : ControllerBase
 {
+    private readonly IUnitContactPointsService _contactPointService;
+
     /// <summary>
-    /// Controller for unit profile contact point API endpoints for internal consumption (e.g. Notifications) requiring neither authenticated user token nor access token authorization.
+    /// Initializes a new instance of the <see cref="UnitContactPointController"/> class.
     /// </summary>
-    [Route("profile/api/v1/units/contactpoint")]
-    [ApiExplorerSettings(IgnoreApi = true)]
-    [Consumes("application/json")]
-    [Produces("application/json")]
-    public class UnitContactPointController : ControllerBase
+    public UnitContactPointController(IUnitContactPointsService contactPointsService)
     {
-        private readonly IUnitContactPointsService _contactPointService;
+        _contactPointService = contactPointsService;
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UnitContactPointController"/> class.
-        /// </summary>
-        public UnitContactPointController(IUnitContactPointsService contactPointsService)
-        {
-            _contactPointService = contactPointsService;
-        }
+    /// <summary>
+    /// Endpoint for looking up use registered notification addresses for the provided units in the
+    /// request body.
+    /// </summary>
+    /// <returns>
+    /// Returns a list of user registered notification addresses for the provided units.
+    /// </returns>
+    [HttpPost("lookup")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<UnitContactPointsList>> PostLookup(
+        [FromBody] UnitContactPointLookup unitContactPointLookup)
+    {     
+        Result<UnitContactPointsList, bool> result = 
+            await _contactPointService.GetUserRegisteredContactPoints(unitContactPointLookup);
 
-        /// <summary>
-        /// Endpoint looking up the contact points for the units provided in the lookup object in the request body
-        /// </summary>
-        /// <returns>Returns an overview of the user registered contact points for the provided units and given resource</returns>
-        [HttpPost("lookup")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<UnitContactPointsList>> PostLookup([FromBody] UnitContactPointLookup unitContactPointLookup)
-        {     
-            Result<UnitContactPointsList, bool> result = await _contactPointService.GetUserRegisteredContactPoints(unitContactPointLookup);
-            return result.Match<ActionResult<UnitContactPointsList>>(
-                         success => Ok(success),
-                         _ => Problem("Could not retrieve contact points"));
-        }
+        return result.Match<ActionResult<UnitContactPointsList>>(
+            success => Ok(success),
+            _ => Problem("Could not retrieve contact points"));
     }
 }
