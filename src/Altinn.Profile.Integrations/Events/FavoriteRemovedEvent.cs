38b7a71aacdd00af9dacc8a980764cd9049bf754
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2025-07-14 17:10:23 +0200

    Add own ..Event classes for favorite addition and removal. Use one of them for discovering handlers assembly

---
+++ src/Altinn.Profile.Integrations/Events/FavoriteRemovedEvent.cs
@@ -0,0 +1,12 @@
+namespace Altinn.Profile.Integrations.Events;
+
+/// <summary>
+/// Represents an event that indicates a change in a user's favorites.
+/// </summary>
+/// <param name="UserId">The unique identifier of the user whose favorites has changed. Must be a positive integer.</param>
+/// <param name="PartyUuid">The unique identifier of the party that were added or removed from the user's favorites.</param>
+/// <param name="CreationTimestamp">Creation timestamp for the favorite</param>
+public record FavoriteRemovedEvent(
+    int UserId,
+    Guid PartyUuid,
+    DateTime CreationTimestamp);
