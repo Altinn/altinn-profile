6fb0ca17ecf223c5d7684d93edf479df31e68129
Sigrid <sigrid.bratsberg@digdir.no>
2025-08-22 07:26:18 +0200

    Add changelog repository

---
+++ src/Altinn.Profile.Integrations/Repositories/ChangeLogSyncMetadataRepository.cs
@@ -0,0 +1,54 @@
+﻿using Altinn.Profile.Integrations.Persistence;
+using Altinn.Profile.Integrations.SblBridge.Changelog;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Altinn.Profile.Integrations.Repositories;
+
+/// <summary>
+/// Defines a repository for operations related to Changelog sync metadata.
+/// </summary>
+public class ChangelogSyncMetadataRepository(IDbContextFactory<ProfileDbContext> contextFactory) : IChangelogSyncMetadataRepository
+{
+    private readonly IDbContextFactory<ProfileDbContext> _contextFactory = contextFactory;
+
+    /// <inheritdoc />
+    public async Task<DateTime?> GetLatestSyncTimestampAsync(DataType dataType)
+    {
+        using ProfileDbContext databaseContext = await _contextFactory.CreateDbContextAsync();
+
+        var lastSync = await databaseContext.ChangelogSyncMetadata.FirstOrDefaultAsync(e => e.DataType == dataType);
+        if (lastSync == null)
+        {
+            return null;
+        }
+
+        return lastSync.LastChangedDateTime;
+    }
+
+    /// <inheritdoc />
+    public async Task<DateTime> UpdateLatestChangeTimestampAsync(DateTime updated, DataType dataType)
+    {
+        using ProfileDbContext databaseContext = await _contextFactory.CreateDbContextAsync();
+        var lastSync = await databaseContext.ChangelogSyncMetadata.FirstOrDefaultAsync(e => e.DataType == dataType);
+        if (lastSync == null)
+        {
+            lastSync = new Entities.ChangelogSyncMetadata
+            {
+                LastChangedId = Guid.NewGuid().ToString("N"),
+                LastChangedDateTime = updated,
+                DataType = dataType
+            };
+            databaseContext.ChangelogSyncMetadata.Add(lastSync);
+        }
+        else
+        {
+            lastSync.LastChangedDateTime = updated;
+
+            databaseContext.ChangelogSyncMetadata.Update(lastSync);
+        }
+
+        await databaseContext.SaveChangesAsync();
+        return updated;
+    }
+}
