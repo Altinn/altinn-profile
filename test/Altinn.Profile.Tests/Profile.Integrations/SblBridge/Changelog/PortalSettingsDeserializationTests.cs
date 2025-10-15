364f674e49e1caa5f885e8b4a6c83b458e3beb9e
Sigrid <sigrid.bratsberg@digdir.no>
2025-10-15 10:20:31 +0200

    Update test

--- test/Altinn.Profile.Tests/Profile.Integrations/SblBridge/Changelog/PortalSettingsDeserializationTests.cs
+++ test/Altinn.Profile.Tests/Profile.Integrations/SblBridge/Changelog/PortalSettingsDeserializationTests.cs
@@ -12,11 +12,11 @@ namespace Altinn.Profile.Tests.Profile.Integrations.SblBridge.Changelog
             var json = @"{
                 ""userId"": 20000018,
                 ""languageType"": 1044,
-                ""doNotPromptForParty"": 0,
+                ""doNotPromptForParty"": false,
                 ""preselectedPartyUuid"": """",
-                ""showClientUnits"": 0,
-                ""shouldShowSubEntities"": 1,
-                ""shouldShowDeletedEntities"": 0,
+                ""showClientUnits"": false,
+                ""shouldShowSubEntities"": true,
+                ""shouldShowDeletedEntities"": false,
                 ""ignoreUnitProfileDateTime"": """"
             }";
 
