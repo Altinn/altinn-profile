8fb16ed2373379204fd77d74b40f416a38afcc46
Suhasini Narasimharaj <aas-snara@DEV-AAS-TJE-48>
2025-10-30 10:02:39 +0000

    Created a new Dashboard Controller

---
+++ src/Altinn.Profile/Controllers/DashboardController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Altinn.Profile.Authorization;
+using Altinn.Profile.Core.OrganizationNotificationAddresses;
+using Altinn.Profile.Mappers;
+using Altinn.Profile.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.Profile.Controllers
+{
+    /// <summary>
+    /// Controller for organization notifications address API endpoints for external usage
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="DashboardController"/> class.
+    /// </remarks>
+    [Route("profile/api/v1/dashboard/organizations/{organizationNumber}/notificationaddresses")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    public class DashboardController(IOrganizationNotificationAddressesService notificationAddressService) : ControllerBase
+    {
+        private readonly IOrganizationNotificationAddressesService _notificationAddressService = notificationAddressService;
+
+        /// <summary>
+        /// Endpoint looking up the notification addresses for the given organization
+        /// </summary>
+        /// <returns>Returns an overview of the registered notification addresses for the provided organization</returns>
+        [HttpGet("mandatory")]
+        [Authorize(Policy = AuthConstants.OrgNotificationAddress_Read)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<OrganizationResponse>> GetMandatory([FromRoute]string organizationNumber, CancellationToken cancellationToken)
+        {
+            var organizations = await _notificationAddressService.GetOrganizationNotificationAddresses([organizationNumber], cancellationToken);
+
+            var orgCount = organizations.Count();
+
+            if (orgCount == 0)
+            {
+                return NotFound();
+            }
+            else if (orgCount > 1)
+            {
+                throw new InvalidOperationException("Indecisive organization result");
+            }
+
+            var organization = organizations.First();
+
+            var response = OrganizationResponseMapper.ToOrganizationResponse(organization);
+
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Endpoint looking up a specific notification address for the given organization
+        /// </summary>
+        /// <returns>Returns the notification addresses for the provided organization</returns>
+        [HttpGet("mandatory/{notificationAddressId}")]
+        [Authorize(Policy = AuthConstants.OrgNotificationAddress_Read)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<NotificationAddressResponse>> GetMandatoryNotificationAddress([FromRoute] string organizationNumber, [FromRoute] int notificationAddressId, CancellationToken cancellationToken)
+        {
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            var organizations = await _notificationAddressService.GetOrganizationNotificationAddresses([organizationNumber], cancellationToken);
+
+            var orgCount = organizations.Count();
+
+            if (orgCount == 0)
+            {
+                return NotFound();
+            }
+            else if (orgCount > 1)
+            {
+                throw new InvalidOperationException("Indecisive organization result");
+            }
+
+            var organization = organizations.First();
+            var notificationAddress = organization.NotificationAddresses.FirstOrDefault(n => n.NotificationAddressID == notificationAddressId);
+
+            if (notificationAddress == null)
+            {
+                return NotFound();
+            }
+
+            var response = OrganizationResponseMapper.ToNotificationAddressResponse(notificationAddress);
+
+            return Ok(response);
+        }       
+    }
+}
