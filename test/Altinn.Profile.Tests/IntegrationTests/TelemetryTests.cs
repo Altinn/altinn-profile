42689ed5a3cd7f38ae95480e2244f99377858b4d
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2025-11-06 16:31:26 +0100

    Alternative disposal of MeterProvider

--- test/Altinn.Profile.Tests/IntegrationTests/TelemetryTests.cs
+++ test/Altinn.Profile.Tests/IntegrationTests/TelemetryTests.cs
@@ -11,18 +11,16 @@ using Xunit;
 namespace Altinn.Profile.Tests.IntegrationTests;
 
 public class TelemetryTests(ProfileWebApplicationFactory<Program> factory) 
-    : IClassFixture<ProfileWebApplicationFactory<Program>>, IDisposable
+    : IClassFixture<ProfileWebApplicationFactory<Program>>
 {
     private readonly ProfileWebApplicationFactory<Program> _factory = factory;
-
-    private MeterProvider _meterProvider;
 
     [Fact]
     public async Task SyncPersonChanges_WhenCalled_CreatesContactRegistryMetrics()
     {
         var metricItems = new List<Metric>();
 
-        _meterProvider = Sdk.CreateMeterProviderBuilder()
+        using var meterProvider = Sdk.CreateMeterProviderBuilder()
             .AddMeter("platform-profile")
             .AddInMemoryExporter(metricItems)
             .Build();
@@ -38,8 +36,6 @@ public class TelemetryTests(ProfileWebApplicationFactory<Program> factory)
         // In unit tests environment there may be a lot of parallel unit tests executed, so
         // giving some breezing room for the End callback to complete
         await Task.Delay(TimeSpan.FromSeconds(1));
-
-        _meterProvider.Dispose();
 
         var addedMetrics = metricItems
             .Where(item => item.Name == "profile.contactregistry.person.added")
@@ -58,7 +54,7 @@ public class TelemetryTests(ProfileWebApplicationFactory<Program> factory)
     {
         var metricItems = new List<Metric>();
 
-        _meterProvider = Sdk.CreateMeterProviderBuilder()
+        using var meterProvider = Sdk.CreateMeterProviderBuilder()
             .AddMeter("platform-profile")
             .AddInMemoryExporter(metricItems)
             .Build();
@@ -71,11 +67,9 @@ public class TelemetryTests(ProfileWebApplicationFactory<Program> factory)
         }
 
         // We need to let End callback execute as it is executed AFTER response was returned.
-        // In unit tests environment there may be a lot of parallel unit tests executed, so
+        // In unit tests environment there may be a lot of parallel unit tests generated, so
         // giving some breezing room for the End callback to complete
         await Task.Delay(TimeSpan.FromSeconds(1));
-
-        _meterProvider.Dispose();
 
         var addedOrgMetrics = metricItems
             .Where(item => item.Name == "profile.organizationnotificationaddress.organization.added")
@@ -97,11 +91,5 @@ public class TelemetryTests(ProfileWebApplicationFactory<Program> factory)
         Assert.Single(addedMetrics);
         Assert.Single(updatedMetrics);
         Assert.Single(deletedMetrics);
-    }
-
-    public void Dispose()
-    {
-        _meterProvider?.Dispose();
-        GC.SuppressFinalize(this);
     }
 }
