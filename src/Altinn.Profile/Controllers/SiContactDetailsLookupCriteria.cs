fa050cbf449775c9aeb43e9040d6818a13d0096e
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2026-02-04 09:45:03 +0100

    Fix duplicated code

--- src/Altinn.Profile/Controllers/SiContactDetailsLookupCriteria.cs
+++ src/Altinn.Profile/Controllers/SiContactDetailsLookupCriteria.cs
@@ -1,25 +1,12 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
-using System.Collections.Generic;
 
 namespace Altinn.Profile.Controllers;
 
 /// <summary>
 /// Represents the criteria used to look up contact details for self-identified users by their email identifiers.
 /// </summary>
-public class SiContactDetailsLookupCriteria
-{
-    /// <summary>
-    /// A list of email identifiers for which to retrieve contact points for self-identified users.
-    /// </summary>
-    [JsonRequired]
-    [Required]
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-using System.Collections.Generic;
-
-namespace Altinn.Profile.Controllers;
-
 public class SiContactDetailsLookupCriteria
 {
     /// <summary>
