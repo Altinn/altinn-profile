98b25edd802830175fbf8a21f77e0beab5a63f81
Sigrid <sigrid.bratsberg@digdir.no>
2026-02-11 14:25:32 +0100

    Remove empty string default value

--- src/Altinn.Profile/Models/VerifiedAddressResponse.cs
+++ src/Altinn.Profile/Models/VerifiedAddressResponse.cs
@@ -12,18 +12,18 @@ namespace Altinn.Profile.Models
         /// <summary>
         /// Gets or sets the address that has been verified. This could be an email address or a phone number, depending on the type of address being verified.
         /// </summary>
-        public string Value { get; set; } = string.Empty;
+        public required string Value { get; init; }
 
         /// <summary>
         /// Gets or sets the type of the verified address. This indicates whether the address is an email address or a phone number.
         /// </summary>
         [JsonConverter(typeof(JsonStringEnumConverter))]
-        public AddressType Type { get; set; }
+        public AddressType Type { get; init; }
 
         /// <summary>
         /// Gets or sets the verification status of the address. This indicates whether the address has been successfully verified or not.
         /// </summary>
         [JsonConverter(typeof(JsonStringEnumConverter))]
-        public VerificationType VerificationStatus { get; set; }
+        public VerificationType VerificationStatus { get; init; }
     }
 }
