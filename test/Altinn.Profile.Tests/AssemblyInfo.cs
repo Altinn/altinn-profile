df5b99233084ebbafd5bd403a65ba5e59f667614
Sigrid <sigrid.bratsberg@digdir.no>
2026-02-13 11:13:29 +0100

    Remove assembly info

--- test/Altinn.Profile.Tests/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using Xunit;
-
-// Temporary mitigation: disable parallel test execution to avoid races caused by shared mutable WebApplicationFactory mocks.
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
