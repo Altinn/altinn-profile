9102847db30eb3358fc96482ecf825a335da9ee6
Sigrid <sigbrats@gmail.com>
2026-02-13 08:13:33 +0100

    Add resource to get all verified addresses for a user (#719)
    * Add resource to get all verified addresses for a user
    
    * Add more tests
    
    * Handle coderabbit comments
    
    * Remove empty string default value

---
+++ src/Altinn.Profile/Controllers/AddressVerificationController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Altinn.Profile.Authorization;
+using Altinn.Profile.Core.AddressVerifications;
+using Altinn.Profile.Models;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.Profile.Controllers
+{
+    /// <summary>
+    /// Controller for logic concerning verification of notification addresses
+    /// </summary>
+    [Authorize]
+    [Route("profile/api/v1/users/current/verification/")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    public class AddressVerificationController : ControllerBase
+    {
+        private readonly IAddressVerificationService _addressVerificationService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddressVerificationController"/> class.
+        /// </summary>
+        public AddressVerificationController(IAddressVerificationService addressVerificationService)
+        {
+            _addressVerificationService = addressVerificationService;
+        }
+
+        /// <summary>
+        /// Get all verified addresses for the current user
+        /// </summary>
+        /// <param name="cancellationToken"> Cancellation token for the operation</param>
+        [HttpGet("verified-addresses")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<ActionResult<IEnumerable<VerifiedAddressResponse>>> Get(CancellationToken cancellationToken)
+        {
+            var validationResult = ClaimsHelper.TryGetUserIdFromClaims(Request.HttpContext, out int userId);
+            if (validationResult != null)
+            {
+                return validationResult;
+            }
+
+            var verifiedAddresses = await _addressVerificationService.GetVerifiedAddressesAsync(userId, cancellationToken);
+            var response = verifiedAddresses.Select(va => new VerifiedAddressResponse { Type = va.AddressType, Value = va.Address, VerificationStatus = va.VerificationType });
+
+            return Ok(response);
+        }
+    }
+}
