bb114bd526fac311f7c8645c0296190d0562c6f2
Sigrid <sigbrats@gmail.com>
2026-02-23 11:51:44 +0100

    Add more docs about rate limiting (#739)
    * Add more docs about rate limiting
    
    * Fix typo

--- src/Altinn.Profile/Controllers/AddressVerificationController.cs
+++ src/Altinn.Profile/Controllers/AddressVerificationController.cs
@@ -59,9 +59,15 @@ namespace Altinn.Profile.Controllers
         /// <summary>
         /// Verify an address for the current user by providing the verification code sent to the address.
         /// </summary>
-        /// <param name="request">The api request containing the aadress and code to verify</param>
+        /// <param name="request">The api request containing the address and code to verify</param>
         /// <param name="cancellationToken"> Cancellation token for the operation</param>
-        /// <remarks>This is rate limited</remarks>
+        /// <remarks>
+        /// This endpoint is rate limited using a sliding window rate limiter to prevent abuse.
+        /// Rate limit: 5 requests per 1 minute window with 6 segments per window. This is per running pod instance, so in a scaled environment the effective rate limit will be higher.
+        /// When the limit is exceeded, a 429 Too Many Requests response is returned.
+        /// For more information about sliding window rate limiting, see:
+        /// https://devblogs.microsoft.com/dotnet/announcing-rate-limiting-for-dotnet/#sliding-window-limit
+        /// </remarks>
         [HttpPost("verify")]
         [EnableRateLimiting("verify-address")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
