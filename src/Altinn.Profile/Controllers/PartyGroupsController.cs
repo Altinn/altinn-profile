7be7d2b2dfa013f10ff19125fd5164e0568a3e26
Sigrid <sigrid.bratsberg@digdir.no>
2026-02-24 14:05:39 +0100

    Update controller

--- src/Altinn.Profile/Controllers/PartyGroupsController.cs
+++ src/Altinn.Profile/Controllers/PartyGroupsController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -30,10 +29,12 @@ namespace Altinn.Profile.Controllers
         /// <summary>
         /// Retrieve all groups for a user
         /// </summary>
+        /// <param name="cancellationToken">Cancellation token for the operation</param>
         /// <returns>All groups for the current user.</returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<IReadOnlyList<GroupResponse>>> Get(CancellationToken cancellationToken)
         {
             var validationResult = ClaimsHelper.TryGetUserIdFromClaims(Request.HttpContext, out int userId);
@@ -58,10 +59,13 @@ namespace Altinn.Profile.Controllers
         /// <summary>
         /// Create a new group for the user
         /// </summary>
-        /// <returns>All groups for the current user.</returns>
+        /// <param name="request">The group creation request containing the group name</param>
+        /// <param name="cancellationToken">Cancellation token for the operation</param>
+        /// <returns>The created group.</returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<GroupResponse>> Create([FromBody]GroupRequest request, CancellationToken cancellationToken)
         {
             if (!ModelState.IsValid)
@@ -85,7 +89,7 @@ namespace Altinn.Profile.Controllers
                 GroupId = groupResponse.GroupId
             };
 
-            return CreatedAtAction(nameof(Get), new { groupResponse.GroupId }, response);
+            return Created($"profile/api/v1/users/current/party-groups/{response.GroupId}", response);
         }
     }
 }
