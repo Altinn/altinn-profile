f8de135887a66cb2ba8b5ac6a5d2489af6c860ab
Sigrid <sigbrats@gmail.com>
2026-03-16 09:47:32 +0100

    Outline consentProfile-client (#788)
    * Outline consentProfile-client
    
    * Add tests
    
    * Update docs
    
    * Switch to delegating handler stub

---
+++ test/Altinn.Profile.Tests/Profile.Integrations/SblBridge/User.PrivateConsent/PrivateConsentClientTests.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Altinn.Profile.Integrations.OrganizationNotificationAddressRegistry;
+using Altinn.Profile.Integrations.SblBridge;
+using Altinn.Profile.Integrations.SblBridge.User.PrivateConsent;
+using Altinn.Profile.Tests.IntegrationTests.Mocks;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using Moq;
+using Moq.Protected;
+
+using Xunit;
+
+namespace Altinn.Profile.Tests.Profile.Integrations.SblBridge.User.PrivateConsent;
+
+public class PrivateConsentClientTests
+{
+    [Fact]
+    public async Task UpdatePrivateConsent_SuccessfulRequest_DoesNotThrow()
+    {
+        var response = new HttpResponseMessage(HttpStatusCode.OK);
+        var client = CreateClient(response, out _);
+
+        var request = GetValidRequest();
+        try
+        {
+            await client.UpdatePrivateConsent(request);
+        }
+        catch
+        {
+            Assert.Fail("Exception was thrown on successful request");
+        }
+    }
+
+    [Theory]
+    [InlineData(HttpStatusCode.InternalServerError)]
+    [InlineData(HttpStatusCode.ServiceUnavailable)]
+    public async Task UpdatePrivateConsent_ServerError_ThrowsInternalServerErrorException(HttpStatusCode statusCode)
+    {
+        var response = new HttpResponseMessage(statusCode);
+        var client = CreateClient(response, out _);
+
+        var request = GetValidRequest();
+        await Assert.ThrowsAsync<InternalServerErrorException>(() => client.UpdatePrivateConsent(request));
+    }
+
+    [Fact]
+    public async Task UpdatePrivateConsent_UnexpectedStatus_LogsError()
+    {
+        var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+        {
+            Content = new StringContent("Bad request error")
+        };
+        var client = CreateClient(response, out var loggerMock);
+
+        var request = GetValidRequest();
+        await client.UpdatePrivateConsent(request);
+
+        loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Unexpected response")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Once);
+    }
+
+    private static PrivateConsentChangedRequest GetValidRequest() => new()
+    {
+        ChangeType = "insert",
+        ChangeDateTime = DateTime.UtcNow,
+        UserId = 123,
+        PhoneNumber = "+4798765432",
+        EmailAddress = "test@test.com"
+    };
+
+    private static PrivateConsentProfileClient CreateClient(HttpResponseMessage response, out Mock<ILogger<PrivateConsentProfileClient>> loggerMock)
+    {
+        HttpClient httpClient;
+        if (response != null)
+        {
+            DelegatingHandlerStub messageHandler = new((request, cancellationToken) => Task.FromResult(response));
+            httpClient = new HttpClient(messageHandler);
+        }
+        else
+        {
+            httpClient = new HttpClient();
+        }
+
+        var settingsMock = new Mock<IOptions<SblBridgeSettings>>();
+        settingsMock.Setup(s => s.Value).Returns(new SblBridgeSettings { ApiProfileEndpoint = "http://localhost/" });
+
+        loggerMock = new Mock<ILogger<PrivateConsentProfileClient>>();
+
+        return new PrivateConsentProfileClient(httpClient, loggerMock.Object, settingsMock.Object);
+    }
+}
