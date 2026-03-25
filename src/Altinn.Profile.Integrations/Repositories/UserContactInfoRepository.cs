e4df31212e82f3f5445e3dfc2148c199f5ea0ff8
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2026-03-25 17:53:21 +0100

    Create skeleton repo

---
+++ src/Altinn.Profile.Integrations/Repositories/UserContactInfoRepository.cs
@@ -0,0 +1,23 @@
+using Altinn.Profile.Core.Integrations;
+using Altinn.Profile.Core.User.ContactInfo;
+using Altinn.Profile.Integrations.Persistence;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Altinn.Profile.Integrations.Repositories;
+
+/// <inheritdoc/>
+/// <summary>
+/// Initializes a new instance of the <see cref="UserContactInfoRepository"/> class.
+/// </summary>
+/// <param name="contextFactory">A factory for creating instances of <see cref="ProfileDbContext"/></param>
+public class UserContactInfoRepository(IDbContextFactory<ProfileDbContext> contextFactory) : IUserContactInfoRepository
+{
+    private IDbContextFactory<ProfileDbContext> _contextFactory = contextFactory;
+
+    /// <inheritdoc/>
+    public Task<UserContactInfo?> UpdateMobileNumber(int userId, string phoneNumber)
+    {
+        throw new NotImplementedException();
+    }
+}
