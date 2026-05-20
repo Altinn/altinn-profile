e55819ca1103d3cff57d922bf8451affc3da0232
Sigrid <sigrid.bratsberg@digdir.no>
2026-05-20 10:18:05 +0200

    Include migration again

--- src/Altinn.Profile.Integrations/Migrations/20260507125218_InitialMigration.cs
+++ src/Altinn.Profile.Integrations/Migrations/20260507125218_InitialMigration.cs
@@ -18,7 +18,6 @@ namespace Altinn.Profile.Integrations.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             /* Comment out after initial migration has been applied, to prevent accidental data loss in case of re-application of the migration. */
-            /*
             migrationBuilder.EnsureSchema(
                 name: "lease");
             migrationBuilder.GrantSchemaPermissions("lease");
@@ -492,7 +491,6 @@ namespace Altinn.Profile.Integrations.Migrations
                 table: "verified_addresses",
                 columns: new[] { "user_id", "address", "address_type" },
                 unique: true);
-            */
         }
 
         /// <inheritdoc />
