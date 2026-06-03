cb27eaf5f4daa4c01cb73ad0f88c1c27204b1d0d
Sigrid <sigrid.bratsberg@digdir.no>
2026-06-03 08:37:38 +0200

    Add extra identity check in FeatureToggledScopeAccessHandler

--- src/Altinn.Profile/Authorization/FeatureToggledScopeAccessHandler.cs
+++ src/Altinn.Profile/Authorization/FeatureToggledScopeAccessHandler.cs
@@ -37,6 +37,13 @@ namespace Altinn.Profile.Authorization
         /// <returns>A Task</returns>
         protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, FeatureToggledScopeAccessRequirement requirement)
         {
+            if (context.User?.Identity == null || !context.User.Identity.IsAuthenticated)
+            {
+                context.Fail();
+                await Task.CompletedTask;
+                return;
+            }
+
             string contextScope = context.User?.Identities
                 ?.FirstOrDefault(i => i.AuthenticationType != null && i.AuthenticationType.Equals("AuthenticationTypes.Federation"))?.Claims
                 .Where(c => c.Type.Equals("urn:altinn:scope"))
