9e8bf485349e8d00c533679bceee278226472575
Sigrid <sigbrats@gmail.com>
2026-06-11 13:23:58 +0200

    Alter table receipt settings with new pk (#936)
    * Re-do migration from current main
    
    * Use string.empty
    
    * Remove isPrivate and reintroduce profile type

---
+++ src/Altinn.Profile.Integrations/Migrations/20260610093054_AlterTableReceiptSettings.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Altinn.Profile.Integrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTableReceiptSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "userid_profiletype_pk",
+                schema: "user_preferences",
+                table: "receipt_settings");
+
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                schema: "user_preferences",
+                table: "receipt_settings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "id_pkey",
+                schema: "user_preferences",
+                table: "receipt_settings",
+                column: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "id_pkey",
+                schema: "user_preferences",
+                table: "receipt_settings");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                schema: "user_preferences",
+                table: "receipt_settings");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "userid_profiletype_pk",
+                schema: "user_preferences",
+                table: "receipt_settings",
+                columns: new[] { "user_id", "profile_type" });
+        }
+    }
+}
