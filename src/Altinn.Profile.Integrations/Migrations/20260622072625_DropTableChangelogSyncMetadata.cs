8272efabb17022d39400ca310bbd7ac31aa1eb5f
Sigrid <sigbrats@gmail.com>
2026-06-22 09:43:54 +0200

    Add migration (#960)

---
+++ src/Altinn.Profile.Integrations/Migrations/20260622072625_DropTableChangelogSyncMetadata.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Profile.Integrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropTableChangelogSyncMetadata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "changelog_sync_metadata",
+                schema: "lease");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "changelog_sync_metadata",
+                schema: "lease",
+                columns: table => new
+                {
+                    last_changed_id = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    data_type = table.Column<int>(type: "integer", nullable: false),
+                    last_change_ticks = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("changelog_sync_metadata_pkey", x => x.last_changed_id);
+                });
+        }
+    }
+}
