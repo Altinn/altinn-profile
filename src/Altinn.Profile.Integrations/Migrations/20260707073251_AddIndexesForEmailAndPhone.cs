4633b0ba73c41216c4dccfef40df54fd27207b43
Sigrid <sigrid.bratsberg@digdir.no>
2026-07-07 09:35:40 +0200

    Add migration

---
+++ src/Altinn.Profile.Integrations/Migrations/20260707073251_AddIndexesForEmailAndPhone.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altinn.Profile.Integrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexesForEmailAndPhone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_self_identified_users_email_address",
+                schema: "user_preferences",
+                table: "self_identified_users",
+                column: "email_address");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_self_identified_users_phone_number",
+                schema: "user_preferences",
+                table: "self_identified_users",
+                column: "phone_number");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_person_email_address",
+                schema: "contact_and_reservation",
+                table: "person",
+                column: "email_address");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_person_mobile_phone_number",
+                schema: "contact_and_reservation",
+                table: "person",
+                column: "mobile_phone_number");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_self_identified_users_email_address",
+                schema: "user_preferences",
+                table: "self_identified_users");
+
+            migrationBuilder.DropIndex(
+                name: "ix_self_identified_users_phone_number",
+                schema: "user_preferences",
+                table: "self_identified_users");
+
+            migrationBuilder.DropIndex(
+                name: "ix_person_email_address",
+                schema: "contact_and_reservation",
+                table: "person");
+
+            migrationBuilder.DropIndex(
+                name: "ix_person_mobile_phone_number",
+                schema: "contact_and_reservation",
+                table: "person");
+        }
+    }
+}
