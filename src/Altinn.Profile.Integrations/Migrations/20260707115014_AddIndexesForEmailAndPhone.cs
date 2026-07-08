e5f1fb184af5252549d05e90c6e28e3b756d14c1
Sigrid <sigrid.bratsberg@digdir.no>
2026-07-08 10:06:12 +0200

    Rewrite index application to use "if not exists"

--- src/Altinn.Profile.Integrations/Migrations/20260707115014_AddIndexesForEmailAndPhone.cs
+++ src/Altinn.Profile.Integrations/Migrations/20260707115014_AddIndexesForEmailAndPhone.cs
@@ -10,22 +10,16 @@ namespace Altinn.Profile.Integrations.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateIndex(
-                name: "ix_self_identified_users_phone_number",
-                schema: "user_preferences",
-                table: "self_identified_users",
-                column: "phone_number");
-
-            migrationBuilder.CreateIndex(
-                name: "ix_person_mobile_phone_number",
-                schema: "contact_and_reservation",
-                table: "person",
-                column: "mobile_phone_number");
+            migrationBuilder.Sql(
+                "CREATE INDEX IF NOT EXISTS ix_self_identified_users_phone_number ON user_preferences.self_identified_users (phone_number);");
 
             migrationBuilder.Sql(
-                "CREATE INDEX ix_person_email_address_lower ON  contact_and_reservation.person (lower(email_address));");
+                "CREATE INDEX IF NOT EXISTS ix_person_mobile_phone_number ON contact_and_reservation.person (mobile_phone_number);");
+
             migrationBuilder.Sql(
-                "CREATE INDEX ix_self_identified_users_email_address_lower ON user_preferences.self_identified_users (lower(email_address));");
+                "CREATE INDEX IF NOT EXISTS ix_person_email_address_lower ON  contact_and_reservation.person (lower(email_address));");
+            migrationBuilder.Sql(
+                "CREATE INDEX IF NOT EXISTS ix_self_identified_users_email_address_lower ON user_preferences.self_identified_users (lower(email_address));");
         }
 
         /// <inheritdoc />
