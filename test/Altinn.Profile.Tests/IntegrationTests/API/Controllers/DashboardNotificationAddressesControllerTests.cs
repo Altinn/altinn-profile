2469b3086a7bf56c6e45b53caa920bb58d49d551
Sigrid <sigrid.bratsberg@digdir.no>
2026-07-10 12:32:35 +0200

    Remove unused usings

--- test/Altinn.Profile.Tests/IntegrationTests/API/Controllers/DashboardNotificationAddressesControllerTests.cs
+++ test/Altinn.Profile.Tests/IntegrationTests/API/Controllers/DashboardNotificationAddressesControllerTests.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Net.Mail;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
