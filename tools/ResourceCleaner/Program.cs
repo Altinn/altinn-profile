dcc513a8d575e7b4a89fc5eca462a5cb121da993
Hallgeir Garnes-Gutvik <hallgeir.garnes-gutvik@digdir.no>
2026-09-16 11:51:00 +0200

    Add A2 service code check

--- tools/ResourceCleaner/Program.cs
+++ tools/ResourceCleaner/Program.cs
@@ -22,13 +22,17 @@ dbSettingsSection.Bind(postgreSqlSettings);
 ProfileClient profileClient = new(postgreSqlSettings.ConnectionString, postgreSqlSettings.CommandTimeoutSeconds);
 List<ServiceResource> RR_resources_displayed_to_users = await ResourceRegistryDataLoader.Load("af-included");
 List<ServiceResource> RR_resources_not_displayed = await ResourceRegistryDataLoader.Load("af-excluded");
+List<ServiceResource> RR_resources_with_A2_servicecode_refs = [..RR_resources_displayed_to_users.Where(ResourceHasA2Ref), ..RR_resources_not_displayed.Where(ResourceHasA2Ref)];
+
+static bool ResourceHasA2Ref(ServiceResource resource) => resource.ResourceReferences.Any(r_ref => r_ref.ReferenceSource == "Altinn2" && r_ref.ReferenceType=="ServiceCode");
 
 List<string> distinctProfileResourceIds = await profileClient.GetDistinctUserPartyResourceIdsAsync(); // <-  Get list of distinct Profile resources
 
-// 'Valid' meaning 
 List<string> profileResourceIdsMatchingAFIncludeList = [];
 List<string> profileResourceIdsMatchingAFExcludeList = [];
-List<string> profileResourceIdsWithoutRRMatch = [];
+
+Dictionary<string, List<ServiceResource>> serviceResourcesMatchingRests = [];
+List<string> profileResourceIdsNotMatchingA2Ref = [];
 
 distinctProfileResourceIds.ForEach(pr =>
 {
@@ -43,30 +47,34 @@ distinctProfileResourceIds.ForEach(pr =>
     {
         profileResourceIdsMatchingAFExcludeList.Add(pr);
     }
-    else
+    else // The profile-stored resource-ID does not match any identifier in RRs resource-list. Check for A2 references
     {
-        profileResourceIdsWithoutRRMatch.Add(pr);
+        List<ServiceResource> A2ServiceCodeRefMatches = RR_resources_with_A2_servicecode_refs.FindAll(serviceResource => serviceResource.ResourceReferences.Any(r_ref => r_ref.ReferenceSource == "Altinn2" && r_ref.ReferenceType=="ServiceCode" && string.eq r_ref.Reference == pr));
+        serviceResourcesMatchingRests[pr] = A2ServiceCodeRefMatches;
     }
 });
 
 string bold = "\x1b[1m";
 string reset = "\x1b[0m";
-
 StringBuilder resultStringBuilder = new();
-resultStringBuilder.AppendLine($"{bold}{"Count of distinct ProfileDB resource-IDs: ", -80}{reset}{distinctProfileResourceIds.Count,10:D}");
-resultStringBuilder.AppendLine($"{bold}{"Count of ProfileDB resource-IDs that match AFs include-list: ", -80}{reset}{profileResourceIdsMatchingAFIncludeList.Count,10:D}");
-resultStringBuilder.AppendLine($"{bold}{"Count of ProfileDB resource-IDs that match AFs exclude-list: ", -80}{reset}{profileResourceIdsMatchingAFExcludeList.Count,10:D}");
-resultStringBuilder.AppendLine($"{bold}{"Count of ProfileDB resource-IDs that have no match in RRs resourcelist (*): ", -80}{reset}{profileResourceIdsWithoutRRMatch.Count,10:D}");
+resultStringBuilder.AppendLine($"{bold}{"Count of distinct ProfileDB resource-IDs: ", -90}{reset}{distinctProfileResourceIds.Count,10:D}");
+resultStringBuilder.AppendLine($"{bold}{"Count of ProfileDB resource-IDs that match AFs include-list: ", -90}{reset}{profileResourceIdsMatchingAFIncludeList.Count,10:D}");
+resultStringBuilder.AppendLine($"{bold}{"Count of ProfileDB resource-IDs that match AFs exclude-list: ", -90}{reset}{profileResourceIdsMatchingAFExcludeList.Count,10:D}");
 resultStringBuilder.AppendLine();
-resultStringBuilder.AppendLine("* likely A2 service codes");
+resultStringBuilder.AppendLine($"{bold}{"Count of ProfileDB resource-IDs that have no match in RRs resourcelist: ", -90}{reset}{serviceResourcesMatchingRests.Keys.Count,10:D}");
+resultStringBuilder.AppendLine($"{bold}{".. of which, we can map a number of: ", -90}{reset}{serviceResourcesMatchingRests.Count(kvPair => kvPair.Value.Count > 0),10:D}");
+resultStringBuilder.AppendLine($"{bold}{"  .. where these have a single mapping candidate: ", -90}{reset}{serviceResourcesMatchingRests.Count(kvPair => kvPair.Value.Count == 1),10:D}");
+resultStringBuilder.AppendLine($"{bold}{"  .. and these have a several mapping candidates: ", -90}{reset}{serviceResourcesMatchingRests.Count(kvPair => kvPair.Value.Count > 1),10:D}");
+resultStringBuilder.AppendLine($"{bold}{".. while the rest count (neither matching RR identifier or A2 service code ref) is: ", -90}{reset}{serviceResourcesMatchingRests.Count(kvPair => kvPair.Value.Count == 0),10:D}");
+
 Console.Write(resultStringBuilder);
 
 Console.WriteLine();
-Console.WriteLine($"{bold}Profile-stored resource-IDs in AFs exclude list:{reset}");
-profileResourceIdsMatchingAFExcludeList.Sort();
-profileResourceIdsMatchingAFExcludeList.ForEach(r =>
+Console.WriteLine($"{bold}{"Service codes with several mapping candidates: ", -90}{reset}");
+foreach (var kvPair in serviceResourcesMatchingRests)
 {
-   Console.WriteLine(r);
-});
-
-//  rrResourceList.Any(r => r.Identifier == profileResourceId)
+    if (kvPair.Value.Count > 1)
+    {
+        Console.WriteLine(kvPair.Key);
+    }
+}
