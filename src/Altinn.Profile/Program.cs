59ff6c5bafbdacba3a8a7c4496c26cff06e53929
Normann, Henning <acn-hnorm@ai-dev.no>
2026-09-21 09:14:05 +0200

    Avoid azure monitor exporter sampling

--- src/Altinn.Profile/Program.cs
+++ src/Altinn.Profile/Program.cs
@@ -232,14 +232,20 @@ static void AddAzureMonitorTelemetryExporters(IServiceCollection services, IConf
     services.Configure<OpenTelemetryLoggerOptions>(logging => logging.AddAzureMonitorLogExporter(o =>
     {
         o.ConnectionString = applicationInsightsConnectionString;
+        o.SamplingRatio = 1.0f;
+        o.TracesPerSecond = null;
     }));
     services.ConfigureOpenTelemetryMeterProvider(metrics => metrics.AddAzureMonitorMetricExporter(o =>
     {
         o.ConnectionString = applicationInsightsConnectionString;
+        o.SamplingRatio = 1.0f;
+        o.TracesPerSecond = null;
     }));
     services.ConfigureOpenTelemetryTracerProvider(tracing => tracing.AddAzureMonitorTraceExporter(o =>
     {
         o.ConnectionString = applicationInsightsConnectionString;
+        o.SamplingRatio = 1.0f;
+        o.TracesPerSecond = null;
     }));
 }
 
